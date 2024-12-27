82ead5b9ee495d940df59ad8c795f06c35704df5
jamlotrasoiaf <89862130+jamlotrasoiaf@users.noreply.github.com>
2024-12-27 12:27:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 # okay im just here to say FISH DIE. RUINING MY PRETTY SCRIPT. MF
 
 #install basics
-sudo pacman -S --needed base-devel make cmake git wget vulkan-tools mesa-utils meson clang gcc python python-packaging python-mako python-yaml flex bison
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm base-devel make cmake git wget vulkan-tools mesa-utils meson clang gcc python python-packaging python-mako python-yaml flex bison
 
 # get latest version and extract it
 wget https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.tar.gz
@@ -21,7 +22,7 @@ patch -p1 < fix-for-getprogname.patch
 rm *.patch
 
 # install build dependencies
-sudo pacman -S --needed zlib expat libdrm libx11 libxcb libxext libxdamage libxshmfence libxxf86vm libxrandr wayland wayland-protocols egl-wayland
+sudo pacman -S --needed --noconfirm zlib expat libdrm libx11 libxcb libxext libxdamage libxshmfence libxxf86vm libxrandr wayland wayland-protocols egl-wayland
 
 # build and install
 meson setup build -Dprefix=/usr -Dcpp_rtti=false -Dgbm=disabled -Dopengl=false -Dllvm=disabled -Dshared-llvm=disabled -Dplatforms=x11,wayland -Dgallium-drivers= -Dxmlconfig=disabled -Dvulkan-drivers=freedreno -Dfreedreno-kmds=msm,kgsl
